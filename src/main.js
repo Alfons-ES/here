68a3fd9aa9310b2ffb11594dba094969ea66d41e
Alfons-ES <alfonsersv@gmail.com>
2026-03-05 09:56:52 +0100

    fungerande markörer för fornlämningar!

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-//sätt ut markör för var vi är.
+//hittat vart avnänderan befinner sig, sätt ut markör för var vi är, kalla fornlämnings findern
 map.on('locationfound', function (e) {
     loading.style.display = 'none';
     const latlng = e.latlng;
@@ -33,8 +33,9 @@ map.on('locationfound', function (e) {
     } else {
         userMarker = L.marker(latlng, { icon: userIcon })
             .addTo(map)
-
     }
+    findFL();
+
 });
 
 document.getElementById('userLocation').addEventListener('click', () => {
@@ -81,8 +82,6 @@ async function findLocation(location) {
             <p>${data[0].display_name}</p>
             <p>${data[0].addresstype}</p>
             `
-
-
         } else {
             alert('Hittade inte platsen, var mer specifik!');
         }
@@ -103,3 +102,76 @@ document.getElementById('inputLocation').addEventListener('keypress', function (
         findLocation(location);
     }
 });
+
+
+function findFL() {
+    fetch("https://kulturarvsdata.se/ksamsok/api?method=search&hitsPerPage=25&query=boundingBox=/WGS84%20%2214.901%2059.259%2014.991%2059.349%22%20AND%20text=fornlämning", {
+        headers: { 'Accept': 'application/json' }
+    })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+
+            if (window.flMarkers) {
+                window.flMarkers.forEach(m => map.removeLayer(m));
+            }
+            window.flMarkers = [];
+
+            let records = data.result.records || [];
+
+            records.forEach((rec, i) => {
+                let graph = rec.record["@graph"] || [];
+
+
+                let coordsValue;
+                for (let node of graph) {
+                    if (node["ksam:coordinates"]) {
+                        coordsValue = node["ksam:coordinates"];
+                        break;
+                    }
+                }
+
+
+                // ta strängen 
+                let xml = coordsValue["@value"];
+
+
+                // plocka ut alla nummer-par med regex (lon,lat)
+                let coords = [];
+                let regex = /([\d.]+),([\d.]+)/g;
+                let match;
+                while (match = regex.exec(xml)) {
+                    let lon = parseFloat(match[1]);
+                    let lat = parseFloat(match[2]);
+                    coords.push([lat, lon]); // leaflet vill ha lat först
+                }
+
+                //medelpunkt
+                let sumLat = 0, sumLon = 0;
+                coords.forEach(c => {
+                    sumLat += c[0];
+                    sumLon += c[1];
+                });
+                let center = [sumLat / coords.length, sumLon / coords.length];
+
+
+                // gör prick
+                let prick = L.marker(center, {
+                    icon: L.divIcon({
+                        html: '<p>X</p>',
+                        className: "",
+                        iconSize: [20, 20],
+                        iconAnchor: [10, 10]
+                    })
+                }).addTo(map);
+
+
+
+                window.flMarkers = window.flMarkers || [];
+                window.flMarkers.push(prick);
+            });
+
+        })
+        .catch(error => console.log(error));
+}
+
